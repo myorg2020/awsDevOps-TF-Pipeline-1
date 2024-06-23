67e7946e354c80dd937a167f93d6367dfa4b7c55
arops2020 <amitesh1258@gmail.com>
2024-06-23 22:38:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf16112023"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
